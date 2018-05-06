1f10f4c5238ddc8d4d64c44772560469365507f7
Miguel Guilherme <mguilherme82@gmail.com>
2018-05-06 20:27:44 +0100

    add infix to last case verify

--- password-verifier/src/main/scala/guilherme/miguel/PasswordVerifier.scala
+++ password-verifier/src/main/scala/guilherme/miguel/PasswordVerifier.scala
@@ -17,11 +17,12 @@ class PasswordVerifier {
     case s if s.length <= 8 => throwException("password should be larger than 8 chars")
     case s if s equals s.toLowerCase => throwException("password should have one uppercase letter at least")
     case s if s equals s.toUpperCase => throwException("password should have one lowercase letter at least")
-    case s if s.exists(!_.isDigit) => throwException("password should have one number at least")
+    case s if s exists (!_.isDigit) => throwException("password should have one number at least")
   }
 
   /**
     * Throws an IllegalArgumentException with a given message.
+    *
     * @param message the message
     * @return an IllegalArgumentException
     */
