44d99111ac0d5e81f18fe3891e894a5a0e7f8e9f
Miguel Guilherme <mguilherme82@gmail.com>
2018-05-06 20:23:28 +0100

    password verifier test refactor

--- password-verifier/src/test/scala/guilherme/miguel/PasswordVerifierTest.scala
+++ password-verifier/src/test/scala/guilherme/miguel/PasswordVerifierTest.scala
@@ -9,35 +9,40 @@ import org.scalatest.{BeforeAndAfterEach, FlatSpec, Matchers}
   */
 class PasswordVerifierTest extends FlatSpec with Matchers with BeforeAndAfterEach {
 
-  var passwordVerifier: PasswordVerifier = _
+  var pwdVerifier: PasswordVerifier = _
 
-  override protected def beforeEach(): Unit = passwordVerifier = new PasswordVerifier
+  override protected def beforeEach(): Unit = pwdVerifier = new PasswordVerifier
 
   behavior of "PasswordVerifier"
 
   it should "throw: password should be larger than 8 chars" in {
-    val thrown = the[IllegalArgumentException] thrownBy passwordVerifier.verify("Secret1")
-    thrown.getMessage should equal("password should be larger than 8 chars")
+    the[IllegalArgumentException] thrownBy {
+      pwdVerifier verify "Secret1"
+    } should have message "password should be larger than 8 chars"
   }
 
   it should "throw: password should not be null" in {
-    val thrown = the[IllegalArgumentException] thrownBy passwordVerifier.verify(null)
-    thrown.getMessage should equal("password should not be null")
+    the[IllegalArgumentException] thrownBy {
+      pwdVerifier verify null
+    } should have message "password should not be null"
   }
 
   it should "throw: password should have one uppercase letter at least" in {
-    val thrown = the[IllegalArgumentException] thrownBy passwordVerifier.verify("mypassword")
-    thrown.getMessage should equal("password should have one uppercase letter at least")
+    the[IllegalArgumentException] thrownBy {
+      pwdVerifier verify "mypassword"
+    } should have message "password should have one uppercase letter at least"
   }
 
   it should "throw: password should have one lowercase letter at least" in {
-    val thrown = the[IllegalArgumentException] thrownBy passwordVerifier.verify("MYPASSWORD")
-    thrown.getMessage should equal("password should have one lowercase letter at least")
+    the[IllegalArgumentException] thrownBy {
+      pwdVerifier verify "MYPASSWORD"
+    } should have message "password should have one lowercase letter at least"
   }
 
   it should "throw: password should have one number at least" in {
-    val thrown = the[IllegalArgumentException] thrownBy passwordVerifier.verify("myPassword")
-    thrown.getMessage should equal("password should have one number at least")
+    the[IllegalArgumentException] thrownBy {
+      pwdVerifier verify "myPassword"
+    } should have message "password should have one number at least"
   }
 
 }
